16cd7465fd220f34b974bc2e465617dc4fe50261
boshikk <boshikk12345@gmail.com>
2023-10-20 23:33:32 -0600

    changes

--- index.js
+++ index.js
@@ -15,8 +15,13 @@ const finalMessage = message1 + message2 + message3;
 console.log(finalMessage);
 
 // PART 2
-function sliceNumber(num) {
-  return num.toString().split("").join(" ");
-}
 
-sliceNumber(23456);
+// function sliceNumber(num) {
+//   return num.toString().split("").join(" ");
+// }
+
+// sliceNumber(23456);
+
+const num = 12345;
+const slicedNum = num.toString().split("").join(" ");
+console.log(slicedNum);
