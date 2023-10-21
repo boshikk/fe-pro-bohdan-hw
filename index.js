ecb84d3562d56e66f3cd564ab749b5726121e308
boshikk <boshikk12345@gmail.com>
2023-10-20 23:29:14 -0600

    hw3 is done

---
+++ index.js
@@ -0,0 +1,22 @@
+// PART 1
+// function concateMessage() {
+//   const message1 = prompt("Type smth in here :)");
+//   const message2 = prompt("Type smth else in here :)");
+//   const message3 = prompt("Type another message :)");
+//   const finalMessage = message1 + message2 + message3;
+//   return finalMessage;
+// }
+// concateMessage();
+
+const message1 = prompt("Type smth in here :)");
+const message2 = prompt("Type smth else in here :)");
+const message3 = prompt("Type another message :)");
+const finalMessage = message1 + message2 + message3;
+console.log(finalMessage);
+
+// PART 2
+function sliceNumber(num) {
+  return num.toString().split("").join(" ");
+}
+
+sliceNumber(23456);
