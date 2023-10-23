66a39899306514d58b04c09db0f8209b69295c4f
boshikk <boshikk12345@gmail.com>
2023-10-23 00:45:03 -0600

    final

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 function addNumbers() {
-  const firstNumber = prompt("Type a number");
-  const secondNumber = prompt("Type one more number");
+  const firstNumber = Number(prompt("Type a number"));
+  const secondNumber = Number(prompt("Type one more number"));
   return alert(firstNumber + secondNumber);
 }
 addNumbers();
 
 function subtractNumbers() {
-  const firstNumber = prompt("Type a number");
-  const secondNumber = prompt("Type one more number");
+  const firstNumber = Number(prompt("Type a number"));
+  const secondNumber = Number(prompt("Type one more number"));
   return alert(firstNumber - secondNumber);
 }
 subtractNumbers();
 
 function multiplyNumbers() {
-  const firstNumber = prompt("Type a number");
-  const secondNumber = prompt("Type one more number");
+  const firstNumber = Number(prompt("Type a number"));
+  const secondNumber = Number(prompt("Type one more number"));
   return alert(firstNumber * secondNumber);
 }
 multiplyNumbers();
 
 function divideNumbers() {
-  const firstNumber = prompt("Type a number");
-  const secondNumber = prompt("Type one more number");
+  const firstNumber = Number(prompt("Type a number"));
+  const secondNumber = Number(prompt("Type one more number"));
   return alert(firstNumber / secondNumber);
 }
 divideNumbers();
