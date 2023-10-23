739e019228fc4591832cf695e3c26563a91f35eb
boshikk <boshikk12345@gmail.com>
2023-10-23 01:01:07 -0600

    final

--- index.js
+++ index.js
@@ -4,15 +4,15 @@
 //   const message2 = prompt("Type smth else in here :)");
 //   const message3 = prompt("Type another message :)");
 //   const finalMessage = message1 + message2 + message3;
-//   return finalMessage;
+//   return alert(finalMessage);
 // }
 // concateMessage();
 
-const message1 = prompt("Type smth in here :)");
-const message2 = prompt("Type smth else in here :)");
-const message3 = prompt("Type another message :)");
+const message1 = prompt("Type a string in here :)");
+const message2 = prompt("Type one more :)");
+const message3 = prompt("And the last one :)");
 const finalMessage = message1 + message2 + message3;
-console.log(finalMessage);
+alert(finalMessage);
 
 // PART 2
 
@@ -20,8 +20,11 @@ console.log(finalMessage);
 //   return num.toString().split("").join(" ");
 // }
 
-// sliceNumber(23456);
+// console.log(sliceNumber(23456));
 
 const num = 12345;
-const slicedNum = num.toString().split("").join(" ");
-console.log(slicedNum);
+// const slicedNum = num.toString().split("").join(" ");
+const string = num.toString();
+
+// console.log(slicedNum);
+console.log(`${string[0]} ${string[1]} ${string[2]} ${string[3]} ${string[4]}`);
