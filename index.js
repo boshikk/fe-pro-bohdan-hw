5489b1a99e1d805067b4221e88a42d4cb99a0d54
boshikk <boshikk12345@gmail.com>
2023-10-24 19:57:07 -0600

    final

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-const mathOperation = prompt("What do you want to do? add, sub, mult or div?");
-const firstNum = Number(prompt("What's the first number?"));
-const secondNum = Number(prompt("What's the second number?"));
-
-if (mathOperation === "add") {
-  alert(firstNum + secondNum);
-} else if (mathOperation === "sub") {
-  alert(firstNum - secondNum);
-} else if (mathOperation === "mult") {
-  alert(firstNum * secondNum);
-} else if (mathOperation === "div") {
-  alert(firstNum / secondNum);
+function averageNumber() {
+  const num1 = Number(prompt("Enter the first number"));
+  const num2 = Number(prompt("Enter the second number"));
+  const num3 = Number(prompt("Enter the third number"));
+  return alert((num1 + num2 + num3) / 3);
 }
+
+averageNumber();
