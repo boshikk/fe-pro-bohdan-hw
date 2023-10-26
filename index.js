6a20da685fa2fbd8489f7604f2e8bd837b01c69e
boshikk <boshikk12345@gmail.com>
2023-10-25 22:39:41 -0600

    final

--- index.js
+++ index.js
@@ -1,41 +1,16 @@
-function userPortrait() {
-  const userBirthYear = Number(prompt("What year were you born?"));
-  if (!userBirthYear) {
-    alert(
-      "That's so sad that you don't want to tell us about your year of birth ;("
-    );
-  }
-  const userCity = prompt("Where do you live?");
-  if (!userCity) {
-    alert(
-      "That's so sad that you don't want to tell us about the city you live in ;("
-    );
-  }
-  const userFavoriteSport = prompt("What's your favorite kind of sport?");
-  if (!userFavoriteSport) {
-    alert(
-      "That's so sad that you don't want to tell us about your favorite sport ;("
-    );
-  }
-  const currentYear = 2023;
-  const userAge = currentYear - userBirthYear;
-  return alert(
-    `${userAge}
-You live in the ${
-      userCity === "Kyiv" || userCity === "Washington" || userCity === "London"
-        ? "capital"
-        : "city"
-    } ${userCity}.
-${
-  userFavoriteSport === "soccer"
-    ? "Cool! You wanna be like Christiano Ronaldo?"
-    : userFavoriteSport === "basketball"
-    ? "Cool! You wanna be like Michael Jordan?"
-    : userFavoriteSport === "tennis"
-    ? "Cool! You wanna be like Novak Djokovic?"
-    : ""
-}`
-  );
-}
+let numOrStr = prompt("input number or string");
+console.log(numOrStr);
 
-userPortrait();
+switch (true) {
+  case numOrStr === null:
+    console.log("ви скасували");
+    break;
+  case numOrStr.trim() === "":
+    console.log("Empty String");
+    break;
+  case isNaN(+numOrStr):
+    console.log("number is Ba_NaN");
+    break;
+  default:
+    console.log("OK!");
+}
