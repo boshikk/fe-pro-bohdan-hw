932ae3fb8ddcee5a5faffc67f629ffaaa94887b9
boshikk <boshikk12345@gmail.com>
2023-10-25 22:19:00 -0600

    final

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-function averageNumber() {
-  const num1 = Number(prompt("Enter the first number"));
-  const num2 = Number(prompt("Enter the second number"));
-  const num3 = Number(prompt("Enter the third number"));
-  return alert((num1 + num2 + num3) / 3);
+function userPortrait() {
+  const userBirthYear = Number(prompt("What year were you born?"));
+  if (!userBirthYear) {
+    alert(
+      "That's so sad that you don't want to tell us about your year of birth ;("
+    );
+  }
+  const userCity = prompt("Where do you live?");
+  if (!userCity) {
+    alert(
+      "That's so sad that you don't want to tell us about the city you live in ;("
+    );
+  }
+  const userFavoriteSport = prompt("What's your favorite kind of sport?");
+  if (!userFavoriteSport) {
+    alert(
+      "That's so sad that you don't want to tell us about your favorite sport ;("
+    );
+  }
+  const currentYear = 2023;
+  const userAge = currentYear - userBirthYear;
+  return alert(
+    `${userAge}
+You live in the ${
+      userCity === "Kyiv" || userCity === "Washington" || userCity === "London"
+        ? "capital"
+        : "city"
+    } ${userCity}.
+${
+  userFavoriteSport === "soccer"
+    ? "Cool! You wanna be like Christiano Ronaldo?"
+    : userFavoriteSport === "basketball"
+    ? "Cool! You wanna be like Michael Jordan?"
+    : userFavoriteSport === "tennis"
+    ? "Cool! You wanna be like Novak Djokovic?"
+    : ""
+}`
+  );
 }
 
-averageNumber();
+userPortrait();
