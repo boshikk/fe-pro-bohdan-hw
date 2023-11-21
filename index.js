198da571786a10d946a3c4c0cbdbf9e1aa95d0c1
Bohdan Dmytrenko <boshikk12345@gmail.com>
2023-11-21 14:24:38 -0700

    final

--- index.js
+++ index.js
@@ -1,59 +1,78 @@
 class Hamburger {
-  constructor(size, filling) {
+  static SIZE_SMALL = {
+    price: 50,
+    calories: 20,
+  };
+
+  static SIZE_BIG = {
+    price: 100,
+    calories: 40,
+  };
+
+  static STUFFING_CHEESE = {
+    price: 10,
+    calories: 20,
+  };
+
+  static STUFFING_SALAD = {
+    price: 20,
+    calories: 5,
+  };
+
+  static STUFFING_POTATO = {
+    price: 15,
+    calories: 10,
+  };
+
+  static TOPPING_MAYO = {
+    price: 20,
+    calories: 5,
+  };
+
+  static TOPPING_SPICE = {
+    price: 15,
+    calories: 0,
+  };
+
+  constructor(size, stuffing) {
     this.size = size;
-    if (this.size === "small") {
-      this.price = 50;
-      this.calories = 20;
-    } else if (this.size === "big") {
-      this.price = 100;
-      this.calories = 40;
-    }
-    this.filling = filling;
-    if (this.filling === "cheese") {
-      this.price += 10;
-      this.calories += 20;
-    } else if (this.filling === "salad") {
-      this.price += 20;
-      this.calories += 5;
-    } else if (this.filling === "fries") {
-      this.price += 15;
-      this.calories += 10;
-    }
+    this.stuffing = stuffing;
+    this.toppings = [];
   }
 
   addTopping(topping) {
-    this.topping = topping;
-    if (this.topping === "mayo") {
-      this.price += 20;
-      this.calories += 10;
-    } else if (this.topping === "herbs") {
-      this.price += 15;
-    }
-  }
-
-  calculatePrice() {
-    console.log(`Price: ${this.price}`);
+    this.toppings.push(topping);
   }
 
   calculateCalories() {
-    console.log(`Calories: ${this.calories}`);
+    const baseCalories = this.size.calories + this.stuffing.calories;
+    const toppingsCalories = this.toppings.reduce(
+      (sum, topping) => sum + topping.calories,
+      0
+    );
+    return `Total calories are: ${baseCalories + toppingsCalories}`;
+  }
+
+  calculatePrice() {
+    const basePrice = this.size.price + this.stuffing.price;
+    const toppingsPrice = this.toppings.reduce(
+      (sum, topping) => sum + topping.price,
+      0
+    );
+    return `Total price is: ${basePrice + toppingsPrice}`;
   }
 }
 
-const fillings = ["cheese", "salad", "fries"];
+const smallHamburger = new Hamburger(
+  Hamburger.SIZE_SMALL,
+  Hamburger.STUFFING_CHEESE
+);
 
-const smallHamburger = new Hamburger("small", fillings[0]);
-smallHamburger.addTopping("mayo");
-smallHamburger.addTopping("mayo");
-smallHamburger.calculateCalories();
-smallHamburger.calculatePrice();
-smallHamburger.addTopping("herbs");
-smallHamburger.calculateCalories();
-smallHamburger.calculatePrice();
-
-const bigHamburger = new Hamburger("big", fillings[2]);
-bigHamburger.calculateCalories();
-bigHamburger.calculatePrice();
-bigHamburger.addTopping("mayo");
-bigHamburger.calculateCalories();
-bigHamburger.calculatePrice();
+console.log(smallHamburger);
+smallHamburger.addTopping(Hamburger.TOPPING_MAYO);
+console.log(smallHamburger);
+console.log(smallHamburger.calculatePrice());
+console.log(smallHamburger.calculateCalories());
+smallHamburger.addTopping(Hamburger.TOPPING_SPICE);
+console.log(smallHamburger.calculatePrice());
+console.log(smallHamburger.calculateCalories());
