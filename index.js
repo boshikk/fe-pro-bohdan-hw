53c3a074c9edc85aa46677e39cd630ddfdf3f6ba
Bohdan Dmytrenko <boshikk12345@gmail.com>
2023-11-23 21:50:48 -0700

    final

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
-class A {
-  read() {
-    console.log("Reading...");
+const colors = [
+  "color-1",
+  "color-2",
+  "color-3",
+  "color-4",
+  "color-5",
+  "color-6",
+  "color-7",
+  "color-8",
+  "color-9",
+  "color-10",
+];
+
+const users = [
+  { name: "Slava", color: "color-4" },
+  { name: "Lena", color: "color-7" },
+];
+
+const addUser = (userName) => {
+  const usedColors = users.map((user) => user.color);
+
+  const availableColors = colors.filter((color) => !usedColors.includes(color));
+
+  if (availableColors.length > 0) {
+    const randomColor =
+      availableColors[Math.floor(Math.random() * availableColors.length)];
+    const newUser = { name: userName, color: randomColor };
+    users.push(newUser);
   }
-}
+};
 
-class B extends A {
-  close() {
-    console.log("Closing...");
-  }
-}
-
-const instanceB = new B();
-
-console.log(instanceB);
-
-console.log("read" in instanceB);
-console.log("close" in instanceB);
-
-console.log(instanceB.hasOwnProperty("read"));
-console.log(instanceB.hasOwnProperty("close"));
-
-const keys = Object.keys(instanceB);
-console.log(keys);
+addUser("Dan");
+addUser("Cindy");
+addUser("Deborah");
+addUser("James");
+addUser("John");
+addUser("Victor");
+addUser("Vlad");
+addUser("Sean");
+addUser("Eren");
+console.log(users);
