08669624927b9098563d040a95ad13ccd3c5d2e6
Bohdan Dmytrenko <boshikk12345@gmail.com>
2023-11-23 20:51:38 -0700

    final

--- index.js
+++ index.js
@@ -1,66 +1,24 @@
-class SuperMath {
-  check(obj) {
-    if (this.isValidOperator(obj.znak)) {
-      if (this.confirmOperation(obj)) {
-        const result = this[obj.znak](obj.x, obj.y);
-        console.log(`Result: ${result}`);
-      }
-    } else {
-      console.log(
-        "Invalid operator. Please enter a valid mathematical operator (+, -, /, *, %)."
-      );
-      this.input();
-    }
-  }
-
-  confirmOperation(obj) {
-    const userResponse = confirm(
-      `Do you want to perform ${obj.znak} with ${obj.x} and ${obj.y}?`
-    );
-
-    if (!userResponse) {
-      this.input();
-    }
-    return userResponse;
-  }
-
-  input() {
-    const newX = parseFloat(prompt("Enter a new value for x:"));
-    const newY = parseFloat(prompt("Enter a new value for y:"));
-    const newOperator = prompt(
-      "Enter a mathematical operator (+, -, /, *, %):"
-    );
-
-    this.check({ x: newX, y: newY, znak: newOperator });
-  }
-
-  isValidOperator(operator) {
-    const validOperators = ["+", "-", "/", "*", "%"];
-    return validOperators.includes(operator);
-  }
-
-  "+"(x, y) {
-    return x + y;
-  }
-
-  "-"(x, y) {
-    return x - y;
-  }
-
-  "/"(x, y) {
-    return x / y;
-  }
-
-  "*"(x, y) {
-    return x * y;
-  }
-
-  "%"(x, y) {
-    return x % y;
+class A {
+  read() {
+    console.log("Reading...");
   }
 }
 
-const mathCalculator = new SuperMath();
-const mathObject = { x: 12, y: 3, znak: "/" };
+class B extends A {
+  close() {
+    console.log("Closing...");
+  }
+}
 
-mathCalculator.check(mathObject);
+const instanceB = new B();
+
+console.log(instanceB);
+
+console.log("read" in instanceB);
+console.log("close" in instanceB);
+
+console.log(instanceB.hasOwnProperty("read"));
+console.log(instanceB.hasOwnProperty("close"));
+
+const keys = Object.keys(instanceB);
+console.log(keys);
