6c734ab1ce9862cf8807a1e54aca79a1bd96aed0
Bohdan Dmytrenko <boshikk12345@gmail.com>
2023-11-30 15:09:19 -0700

    final

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-const array = [1, 2, 3, 4, 5];
-
-// const sum = array.reduce((sum, cur) => sum + cur, 0);
-// console.log(sum);
-
-const myOwnReduce = (arr, cb, initialValue) => {
-  let accumulator = initialValue;
-  for (const element of arr) {
-    accumulator = cb(accumulator, element);
-  }
-  return accumulator;
+const autocompleteFunc = (arr, str) => {
+  return arr.filter((el) => el.includes(str));
 };
-const myCallback = (sum, cur) => sum * cur;
 
-console.log(myOwnReduce(array, myCallback, 1));
+console.log(autocompleteFunc(["aaa", "bbb", "abb"], "aa"));
