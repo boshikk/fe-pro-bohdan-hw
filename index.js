cdb3896e30a40f02a384da5edac216e4158ff1cb
Bohdan Dmytrenko <boshikk12345@gmail.com>
2023-12-08 19:27:18 -0700

    final

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const textField = document.getElementById("text-field");
-const infoDiv = document.getElementById("info-div");
+const imageFileNames = [
+  "1.jpg",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg",
+  "6.jpg",
+  "7.jpg",
+  "8.jpg",
+  "9.jpg",
+];
 
-console.log(textField);
-console.log(infoDiv);
+const randomIndex = Math.floor(Math.random() * imageFileNames.length);
 
-textField.addEventListener("focus", () => (infoDiv.style.display = "block"));
+const randomImageFileName = imageFileNames[randomIndex];
+console.log(randomImageFileName);
 
-textField.addEventListener("blur", () => (infoDiv.style.display = "none"));
+const randomImage = document.getElementById("random-image");
+randomImage.src = `images/${randomImageFileName}`;
