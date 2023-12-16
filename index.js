ecfd83b46eab681e970a09eae9ee34ff2876f7f0
Bohdan Dmytrenko <boshikk12345@gmail.com>
2023-12-15 19:33:18 -0700

    final

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-const table = document.createElement("table");
+const images = document.querySelectorAll(".slider-image");
+let currentImageIndex = 0;
 
-let count = 1;
-for (let i = 0; i < 10; i++) {
-  const row = table.insertRow();
-  for (let j = 0; j < 10; j++) {
-    const cell = row.insertCell();
-    cell.textContent = count++;
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+const showImage = (index) => {
+  images.forEach((image, i) => {
+    if (i === index) {
+      image.style.display = "block";
+    } else {
+      image.style.display = "none";
+    }
+  });
+};
+
+const prevSlide = () => {
+  if (currentImageIndex > 0) {
+    currentImageIndex--;
+    showImage(currentImageIndex);
   }
-}
 
-document.body.appendChild(table);
+  prevBtn.style.display = currentImageIndex === 0 ? "none" : "block";
+  nextBtn.style.display = "block";
+};
+
+const nextSlide = () => {
+  if (currentImageIndex < images.length - 1) {
+    currentImageIndex++;
+    showImage(currentImageIndex);
+  }
+
+  nextBtn.style.display =
+    currentImageIndex === images.length - 1 ? "none" : "block";
+  prevBtn.style.display = "block"; // Always show prevBtn after Next is clicked
+};
+
+prevBtn.style.display = "none";
+
+prevBtn.addEventListener("click", prevSlide);
+nextBtn.addEventListener("click", nextSlide);
+
+showImage(currentImageIndex);
