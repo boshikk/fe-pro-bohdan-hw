858564fe2fbc7b3d00ea24652933c13003c5c4e4
Bohdan Dmytrenko <boshikk12345@gmail.com>
2023-12-15 18:41:27 -0700

    final

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const options = ["😊", "😄", "😃", "😁", "😆"];
 
 const votingContainer = document.getElementById("voting-container");
 
-// Initialize vote counts
 const voteCounts = Array.from({ length: options.length }, () => 0);
 
-// Create and render smiley elements with containers
 options.forEach((option, index) => {
   const smileyContainer = document.createElement("div");
   smileyContainer.classList.add("smiley-container");
@@ -24,16 +22,14 @@ options.forEach((option, index) => {
   votingContainer.appendChild(smileyContainer);
 });
 
-// Function to handle voting
-function vote(index) {
+const vote = (index) => {
   voteCounts[index]++;
   updateVoteCounts();
-}
+};
 
-// Function to update vote counts
-function updateVoteCounts() {
+const updateVoteCounts = () => {
   const voteCountElements = document.querySelectorAll(".vote-count");
   voteCountElements.forEach((element, index) => {
     element.textContent = `Votes: ${voteCounts[index]}`;
   });
-}
+};
