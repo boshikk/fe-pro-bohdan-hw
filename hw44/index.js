7ca21aa7e877d8cf06b43f2c9af964aaa59511ee
Bohdan Dmytrenko <boshikk12345@gmail.com>
2023-12-20 21:18:17 -0700

    in_process

---
+++ hw44/index.js
@@ -0,0 +1,29 @@
+// const buildRoute = (routeString, params) => {
+//   const regex = new RegExp(/(\:)([a-z]+)(\-)(id)/, "g");
+//   const matches = routeString.match(regex);
+//   // console.log(match);
+
+//   const paramsKeys = Object.keys(params);
+
+//   let str;
+
+//   if (paramsKeys.length === 1) {
+//     str = routeString.replace(regex, params[paramsKeys[0]]);
+//     return str;
+//   } else {
+//     for (const key in params) {
+//       str = routeString.replace(regex, params[key]);
+//       return str;
+//     }
+//   }
+// };
+
+// console.log(buildRoute("/user/:user-id/create", { userId: 1 })); // 'user/1/create'
+// // console.log(buildRoute("/user/:user-id/create", { userId: 15 })); // 'user/1/create'
+// console.log(
+//   buildRoute("/:tour-id/:game-id/:player-id/goal", {
+//     tourId: 1,
+//     gameId: 14,
+//     playerId: 125151,
+//   })
+// ); // '/1/14/125151/goal'
