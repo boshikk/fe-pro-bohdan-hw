83d0ced8bfc4ddaa898f5a526473c4d7fefc5753
Bohdan Dmytrenko <boshikk12345@gmail.com>
2023-12-26 16:26:06 -0700

    final

--- hw44/index.js
+++ hw44/index.js
@@ -1,21 +1,11 @@
+const kebabCaseToCamelCase = (string) => {
+  return string.replace(/-./g, (m) => m.toUpperCase()[1]);
+};
+
 const buildRoute = (routeString, params) => {
-  const regex = new RegExp(/(\:)([a-z]+)(\-)(id)/, "g");
-  const matches = routeString.match(regex);
-  // console.log(match);
-
-  const paramsKeys = Object.keys(params);
-
-  let str;
-
-  if (paramsKeys.length === 1) {
-    str = routeString.replace(regex, params[paramsKeys[0]]);
-    return str;
-  } else {
-    for (const key in params) {
-      str = routeString.replace(regex, params[key]);
-      return str;
-    }
-  }
+  return routeString.replace(/:(.*?)\//g, (_, group) => {
+    return params[kebabCaseToCamelCase(group)] + "/";
+  });
 };
 
 console.log(buildRoute("/user/:user-id/create", { userId: 1 })); // 'user/1/create'
